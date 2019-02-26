9397d81600e019d74b67a377129a49b6dd702c59
Donald Sonn <dsonn@Donalds-MBP.fios-router.home>
2019-02-26 09:09:56 -0500

    added search function

--- index.js
+++ index.js
@@ -10,12 +10,18 @@
 // indicates if it's checked off or not.
 // we're pre-adding items to the shopping list so there's
 // something to see when the page first loads.
-const STORE = [
-  {id: cuid(), name: 'apples', checked: false},
-  {id: cuid(), name: 'oranges', checked: false},
-  {id: cuid(), name: 'milk', checked: true},
-  {id: cuid(), name: 'bread', checked: false}
-];
+
+const STORE = {
+  items: [
+    {id: cuid(), name: "apples", checked: false, searched: false},
+    {id: cuid(), name: "oranges", checked: false, searched: false},
+    {id: cuid(), name: "milk", checked: true, searched: false},
+    {id: cuid(), name: "bread", checked: false, searched: false}
+  ],
+  hideCompleted: false,
+  search: false
+};
+
 
 function generateItemHTML(item){
   return `
@@ -33,7 +39,7 @@ function generateItem(item){
 }
 
 function addItemToShoppingList(item){
-  STORE.push(generateItem(item));
+  STORE.items.push(generateItem(item));
 }
 
 function getItemIdFromElement(element){
@@ -43,21 +49,38 @@ function getItemIdFromElement(element){
 }
 
 function toggleCheckedForListItem(id){
-  const item = STORE.find(item => item.id === id);
+  const item = STORE.items.find(item => item.id === id);
   item.checked = !item.checked; 
 }
 
+function toggleSearchForListItem(id){
+  const item = STORE.items.find(item => item.id === id);
+  item.searched = !item.searched; 
+}
+
 function deleteItemFromList(id){
-  const index = STORE.findIndex(item => item.id === id);
+  const index = STORE.items.findIndex(item => item.id === id);
   if (index === -1) return;
-  STORE.splice(index,1);
+  STORE.items.splice(index,1);
 }
 
 function renderShoppingList() {
   // this function will be responsible for rendering the shopping list in
   // the DOM
   console.log('`renderShoppingList` ran');
-  const htmlString = generateItemsHTML(STORE);
+  let filteredItems = STORE.items;
+
+  // if the `hideCompleted` property is true, then we want to reassign filteredItems to a version
+  // where ONLY items with a "checked" property of false are included
+  if (STORE.hideCompleted) {
+    filteredItems = filteredItems.filter(item => !item.checked);
+  }
+
+  if (STORE.search) {
+    filteredItems = filteredItems.filter(item => item.searched);
+  }
+
+  const htmlString = generateItemsHTML(filteredItems);
   $('.js-shopping-list').html(htmlString);
 
 }
@@ -97,10 +120,55 @@ function handleDeleteItemClicked() {
     const id = getItemIdFromElement(e.currentTarget);
     console.log(id);
     deleteItemFromList(id);
-    console.log(STORE);
+    console.log(STORE.items);
     renderShoppingList();
   });
 }
+
+// Toggles the STORE.hideCompleted property
+function toggleHideFilter() {
+  STORE.hideCompleted = !STORE.hideCompleted;
+}
+
+// Places an event listener on the checkbox for hiding completed items
+function handleToggleHideFilter() {
+  $('.js-hide-completed-toggle').on('click', () => {
+    toggleHideFilter();
+    renderShoppingList();
+  });
+}
+
+// Places an event listener on the search bar 
+function handleSearch() {
+  console.log('`handleSearch` ran');
+  $('#js-shopping-list-search').on('submit', function(e){
+    e.preventDefault();
+    STORE.search = true;
+    let matchedCUIDs = cuidLookup($('.js-shopping-list-search').val());
+    matchedCUIDs.map(id => toggleSearchForListItem(id));
+    $('.js-shopping-list-search').val('');
+    renderShoppingList();
+  });
+}
+
+// Looks up CUID values based on search argument and sets search toggle in STORE object to true
+function cuidLookup(search) {
+  let matchedItem = STORE.items.filter(x => x.name.toLowerCase().includes(search.toLowerCase()));
+  return matchedItem.map(items => items.id);
+}
+
+// function handleNewItemSubmit() {
+//   // this function will be responsible for when users add a new shopping list item
+//   console.log('`handleNewItemSubmit` ran');
+//   $('#js-shopping-list-form').on('submit',function(e){
+//     console.log('Submitting...');
+//     e.preventDefault();
+//     console.log(e);
+//     addItemToShoppingList($('.js-shopping-list-entry').val());
+//     $('.js-shopping-list-entry').val('');
+//     renderShoppingList();
+//   });
+// }
 
 // this function will be our callback when the page loads. it's responsible for
 // initially rendering the shopping list, and activating our individual functions
@@ -111,7 +179,8 @@ function handleShoppingList() {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
-
+  handleToggleHideFilter();
+  handleSearch();
 }
 
 // when the page loads, call `handleShoppingList`
